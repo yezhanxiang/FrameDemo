fe466b5fece98f8fc4d24d0b1279394bd5025c73
展祥叶 <zhanxiangye@zhanxiangyedeimac.local>
2016-10-24 10:12:59 +0800

    Initial Commit

---
+++ FrameDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameDemo
+//
+//  Created by 展祥叶 on 16/10/24.
+//  Copyright © 2016年 ye zhanxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrameDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameDemo
+//
+//  Created by 展祥叶 on 16/10/24.
+//  Copyright © 2016年 ye zhanxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
